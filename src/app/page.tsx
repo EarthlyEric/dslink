9c030eb4d757fdd5962a3520727702032e1dde89
EarthlyEric <earthlyeric6@gmail.com>
2024-05-28 23:05:43 +0800

    temp fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,6 @@
 import { useState } from 'react';
 
 export default function Home() {
-  const [url, setUrl] = useState('');
-  const [shortenedUrl, setShortenedUrl] = useState('');
-
-  const handleShortenClick = async () => {
-    try {
-      const response = await fetch('http://127.0.0.1:443/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ url })
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        setShortenedUrl(data.shortenedUrl);
-      } else {
-        console.error('Failed to shorten URL');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
 
   return (
     <div className="min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8 grid grid-cols-1 mx-10 rounded-lg border-2">
@@ -43,13 +20,10 @@ export default function Home() {
             name="url"
             id="url"
             placeholder="https://example.com"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
             className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
           <div className="absolute inset-y-0 right-0 flex items-center">
             <button
-              onClick={handleShortenClick}
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Shorten
@@ -57,11 +31,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-      {shortenedUrl && (
-        <div className="mt-4 text-center">
-          <p>Shortened URL: <a href={shortenedUrl} className="text-blue-500">{shortenedUrl}</a></p>
-        </div>
-      )}
     </div>
   );
 }
