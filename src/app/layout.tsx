2bee13595a8ecca3f85ca25aab13b1e65978a799
EarthlyEric <earthlyeric6@gmail.com>
2024-05-28 14:18:54 +0800

    change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/image/favicon/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="96x96" href="image/favicon/favicon-96x96.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="image/favicon/favicon-16x16.png"/>
-        <link rel="icon" href="image/favicon/favicon.ico"/>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
